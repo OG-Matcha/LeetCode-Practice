9c9270e2c451a564063ba3ca3abca969251e0b7f
OG-Matcha <matchaskill@gmail.com>
2023-05-03 08:29:06 +0800

    upload today's problem

---
+++ Easy/2215. Find the Difference of Two Arrays.py
@@ -0,0 +1,19 @@
+# easy
+
+'''
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+'''
+
+# https://leetcode.com/problems/find-the-difference-of-two-arrays/description/
+
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        return [list(set(nums1) - set(nums2)), list(set(nums2) - set(nums1))]
+
+# Time complexity = O(n) where n is length of input array with bigger length
+# Space complexity = O(n)
