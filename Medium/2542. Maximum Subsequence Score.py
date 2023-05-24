41ca102844bc4fc1a28d3a2fa9a314e3e08a43cc
OG-Matcha <matchaskill@gmail.com>
2023-05-24 09:24:08 +0800

    upload today's problem

---
+++ Medium/2542. Maximum Subsequence Score.py
@@ -0,0 +1,39 @@
+# medium
+
+'''
+You are given two 0-indexed integer arrays nums1 and nums2 of equal length n and a positive integer k. You must choose a subsequence of indices from nums1 of length k.
+
+For chosen indices i0, i1, ..., ik - 1, your score is defined as:
+
+The sum of the selected elements from nums1 multiplied with the minimum of the selected elements from nums2.
+It can defined simply as: (nums1[i0] + nums1[i1] +...+ nums1[ik - 1]) * min(nums2[i0] , nums2[i1], ... ,nums2[ik - 1]).
+Return the maximum possible score.
+
+A subsequence of indices of an array is a set that can be derived from the set {0, 1, ..., n-1} by deleting some or no elements.
+'''
+
+# https://leetcode.com/problems/maximum-subsequence-score/description/
+
+
+import heapq
+
+
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        total = 0
+        res = 0
+        h = []
+
+        for a, b in sorted(zip(nums1, nums2), key=lambda x: -x[1]):
+            heapq.heappush(h, a)
+            total += a
+
+            if len(h) > k:
+                total -= heapq.heappop(h)
+            if len(h) == k:
+                res = max(res, total * b)
+
+        return res
+
+# Time complexity = O(nlogn) where n is the length of nums2
+# Space complexity = O(n)
