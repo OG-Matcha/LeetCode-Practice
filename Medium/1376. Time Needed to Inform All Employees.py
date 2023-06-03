18d2151057532051b8209c7a02b57a534ff866a0
OG-Matcha <matchaskill@gmail.com>
2023-06-03 10:59:00 +0800

    upload today's problem

---
+++ Medium/1376. Time Needed to Inform All Employees.py
@@ -0,0 +1,35 @@
+# medium
+
+'''
+A company has n employees with a unique ID for each employee from 0 to n - 1. The head of the company is the one with headID.
+
+Each employee has one direct manager given in the manager array where manager[i] is the direct manager of the i-th employee, manager[headID] = -1. Also, it is guaranteed that the subordination relationships have a tree structure.
+
+The head of the company wants to inform all the company employees of an urgent piece of news. He will inform his direct subordinates, and they will inform their subordinates, and so on until all employees know about the urgent news.
+
+The i-th employee needs informTime[i] minutes to inform all of his direct subordinates (i.e., After informTime[i] minutes, all his direct subordinates can start spreading the news).
+
+Return the number of minutes needed to inform all the employees about the urgent news.
+'''
+
+# https://leetcode.com/problems/time-needed-to-inform-all-employees/description/
+
+
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        data = [[] for _ in range(n)]
+
+        for i, v in enumerate(manager):
+            if v != -1:
+                data[v].append(i)
+
+        def dfs(i):
+            ans = 0
+            for j in data[i]:
+                ans = max(dfs(j) + informTime[i], ans)
+            return ans
+
+        return dfs(headID)
+
+# Time comeplexity = O(n)
+# Space comeplexity = O(n)
