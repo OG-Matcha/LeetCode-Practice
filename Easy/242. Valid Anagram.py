db161100843041674f87a511335d6bbb46a54041
OG-Matcha <matchaskill@gmail.com>
2023-06-13 19:37:16 +0800

    rewrite with better solution

--- Easy/242. Valid Anagram.py
+++ Easy/242. Valid Anagram.py
@@ -9,14 +9,16 @@ typically using all the original letters exactly once.
 
 # https://leetcode.com/problems/valid-anagram/description/
 
+import collections
+
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
 
-        count_s = defaultdict(int)
-        count_t = defaultdict(int)
+        count_s = collections.defaultdict(int)
+        count_t = collections.defaultdict(int)
 
         for i in range(len(s)):
             count_s[s[i]] += 1
