7fa9e9d8177563eec9e1e2a7cb599d12c1ef0bb5
OG-Matcha <matchaskill@gmail.com>
2023-06-13 21:10:52 +0800

    upload blind 75

---
+++ Blind 75/Two Pointers/125. Valid Palindrome.py
@@ -0,0 +1,43 @@
+# easy
+
+'''
+A phrase is a palindrome if, 
+after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+'''
+
+# https://leetcode.com/problems/valid-palindrome/description/
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+# Time complexity = O(n)
+# Space complexity = O(1)
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        lst = [i.lower() for i in s if i.isalnum()]
+        return lst == lst[::-1]
+
+# Time complexity = O(n)
+# Space complexity = O(n)
