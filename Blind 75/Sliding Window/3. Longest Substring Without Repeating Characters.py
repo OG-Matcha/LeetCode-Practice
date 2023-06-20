53f1d157489231dbe1724ed9e4ddbac0b6887c26
OG-Matcha <matchaskill@gmail.com>
2023-06-20 20:19:34 +0800

    upload blind 75

--- Blind 75/Sliding Window/3. Longest Substring Without Repeating Characters.py
+++ Blind 75/Sliding Window/3. Longest Substring Without Repeating Characters.py
@@ -20,8 +20,26 @@ class Solution:
                 seen.remove(s[left])
                 left += 1
 
-            seen.add(s[r])
-            res = max(res, r - l + 1)
+            seen.add(s[right])
+            res = max(res, right - left + 1)
+
+        return res
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        seen = dict()
+        left = 0
+        res = 0
+
+        for right in range(len(s)):
+            curr = s[right]
+            if curr in seen and seen[curr] >= left:
+                left = seen[curr] + 1
+            else:
+                res = max(res, right - left + 1)
+            seen[curr] = right
 
         return res
 
